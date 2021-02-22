7d5357f8f09e5196926cd447e0323b96f861d193
John Fotouhi <fotouhjo@outlook.com>
2021-02-21 23:51:45 -0600

    Added Background Images to Cells instead of nums

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -6,32 +6,48 @@ class Box extends React.Component{
     constructor(props){
         super(props);
         this.state={
+            x:-0,
+            y:-59,
             covered:true,
             type:"0",
             //shown: url("https://minesweeperonline.com/sprite150.gif")
-            shown: " "
+            shown: ""
         };
     }
     
     render(){
         let curStyle = {
-            background: 'url("https://minesweeperonline.com/sprite150.gif") -96px -59px' ,
-            backgroundpositionx: '-96px',
-            backgroundpositiony: '-59px',
-            width: '24px',
-            height: '24px'
+            //background: 'url("https://minesweeperonline.com/sprite150.gif") -96px -59px' ,
+            backgroundPositionX: this.state.x,//'0',
+            backgroundPositionY: this.state.y//'-59px'
+           // width: '24px',
+           // height: '24px'
         };
         return(
             <button className="box"
             style={curStyle}
             onClick={(e) => {this.setState({covered: false})}}
-            onContextMenu={(e) => {e.preventDefault(); this.setState({shown: '|'})}}
+            onContextMenu={(e) => {e.preventDefault(); this.setState({x: -24}); this.setState({y: -59})}}
             >
                 
                     {
                     (() => {
                         if(!this.state.covered){
-                            this.state.shown = this.state.type;
+                            if(this.state.type === "0"){
+                                //blank
+                                this.setState({x: 0});
+                                this.setState({y: -35});
+                            }
+                            else if(this.state.type == -1){
+                                //bomb
+                                this.setState({x: 96});
+                                this.setState({y: -59});
+                            }
+                            else{
+                                this.state.shown = this.state.type;
+                            }
+                            this.setState({covered: true});
+                            //this.state.shown = this.state.type;
                             //Board.getElementsByClassName('box')[0].style.background = "url(https://minesweeperonline.com/sprite150.gif)";
                         }
                     })(),
